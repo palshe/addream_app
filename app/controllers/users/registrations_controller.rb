76c1a2e9860fd3abaa751de635aed32957770a0c
palshe <151994170+palshe@users.noreply.github.com>
2024-02-23 11:30:25 +0000

    a

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -38,7 +38,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def after_update_path_for(resource)
+    users_show_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "static_pages#home"
+  devise_for :users, controllers:{ registrations: 'users/registrations' }
+  get 'users/show' => 'users#show'
   get '/about', to: 'static_pages#about'
   get '/help' , to: 'static_pages#help'
 end
