00139fe0544b43c0d1d338ef6589b048b6af32e4
palshe <151994170+palshe@users.noreply.github.com>
2024-02-26 07:08:49 +0000

    b

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   devise_for :users, controllers:{ registrations: 'users/registrations' }
-  get 'users/show' => 'users#show'
+  get '/show' => 'users#show'
   get '/about', to: 'static_pages#about'
   get '/help' , to: 'static_pages#help'
 end
